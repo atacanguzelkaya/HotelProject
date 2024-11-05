b74cb66f2fc6a1ba9919a7365de4a13f86a43fcb
atacanguzelkaya <atacangzlky@gmail.com>
2024-11-05 14:29:29 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.DataAccessLayer.Abstract;
+
+public interface IGenericDal<T> where T : class
+{
+	void Insert(T t);
+	void Update(T t);
+	void Delete(T t);
+	List<T> GetList();
+	T GetById(int id);
+}
---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.DataAccessLayer.Concrete;
+
+public class Context:DbContext
+{
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<Staff> Staffs { get; set; }
+    public DbSet<Subscribe> Subscribes { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial catalog=HotelProjectDb;integrated security=true");
+	}
+}
